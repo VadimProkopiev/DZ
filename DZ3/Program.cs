a5569169b72669f6c9de358e4a4db170c3ce2cc0
«Vadim <Hazz74@yandex.ru>
2022-10-31 22:21:03 +0500

    Fisrt commit

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// for (int i=0; i<=10;i=i+2)
+// Console.Write($"{i}");
+
+Console.WriteLine("Введите координату x");
+int x=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите координату y ");
+int y=Convert.ToInt32 ( Console.ReadLine());
+string res;
+if (x>0 && y>0) res = "Это первая четверть";
+else if (x<0 && y>0) res = "Это вторая четверть";
+else if (x<0 && y<0) res = "Это третья четверть";
+else if (x>0 && y<0) res = "Это четвертая четверть";
+else res = "точка находится на оси";
+Console.WriteLine(res);
