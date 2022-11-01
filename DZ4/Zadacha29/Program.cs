a71a2d2ece3e9eb96e55033b7a0d54fba0ca5a62
«Vadim <Hazz74@yandex.ru>
2022-11-02 00:08:11 +0500

    Write Cod Zadacha29

---
+++ DZ4/Zadacha29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+// элементов с клавиатуры и выводит массив на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int lenght = 8;
+    int index = 0;
+    while (index<lenght)
+{
+    Console.Write($"Введите число {index+1} массива. ");
+    collection[index] = Convert.ToInt32 (Console.ReadLine());;
+    index++;
+}
+}
+void PrintArray(int[] array)
+{
+    int count = 8;
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
