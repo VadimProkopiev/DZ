4333decc0e01cb1abc327655bb47f29aeb3efc64
«Vadim <Hazz74@yandex.ru>
2022-11-01 22:58:41 +0500

    Add Cod Seminar4

--- DZ4/Seminar4/Program.cs
+++ DZ4/Seminar4/Program.cs
@@ -28,14 +28,53 @@
 // //SumNumbers(n);
 // Console.WriteLine(SumNumbers2(n));
 
-
+___________________________________________________________________
 // Задача 26: Напишите программу, которая принимает на вход 
 // число и выдаёт количество цифр в числе.
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
 
-Console.WriteLine("Введите любое число, ");
-int n = Convert.ToInt32 (Console.ReadLine());
-int NumLen = (int)Math.Log10(n) + 1;
-Console.WriteLine($"Количество цифр в числе,  {NumLen}");
+// Console.WriteLine("Введите любое число, ");
+// double n = Convert.ToInt32 (Console.ReadLine());
+// int NumLen = (int)Math.Log10(n) + 1;
+// Console.WriteLine($"Количество цифр в числе,  {NumLen}");
+___________ 
+// Console.WriteLine("Введите 5ти значное число: ");
+// int x=Convert.ToInt32(Console.ReadLine()) ;
+
+// // определение длины числа
+// int i=1;
+// // int j=1;
+// int n=1;
+// int iter;
+// // int pol=0; 
+//       while ((x /= 10) >= 1) 
+//     i=i+1;
+//     Console.WriteLine($"Число символов {i}");
+//   //Длина числа равна i;
+___________
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// SumNumbers(num);
+// ___________
+// // int razryad=0;
+// // while (num % 10 >0)
+// // {
+// //     razryad++;
+// //     num/=10;
+// // }
+// // Console.WriteLine(razryad);
+// ___________
+// void SumNumbers(int num)
+// {
+//     int razryad=0;
+//     while (num % 10 >0)
+//     {
+//         razryad++;
+//         num/=10;
+//     }
+//     Console.WriteLine(razryad);
+// }
+___________________________________________________________________
+
