77125cac1bc706a0b002761c05e7aa1e36b45352
«Vadim <Hazz74@yandex.ru>
2022-11-01 23:54:27 +0500

    Write cod seminar4

--- DZ4/Seminar4/Program.cs
+++ DZ4/Seminar4/Program.cs
@@ -103,39 +103,41 @@
 //  заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-// int[] array = new int[8];
-// void FillArray(int[] collection)
-// {
-//     int lenght = 8;
-//     int index = 0;
-//     while (index<lenght)
-// {
-//     collection[index] = new Random().Next(0,2);
-//     index++;
-// }
-// }
-// void PrintArray(int[] array)
-// {
-//     int count = 8;
-//     for ( int i = 0; i < count; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-// FillArray(array);
-// PrintArray(array);
-// int p=0;
-// int e=0;
-// int count = 8;
-// for (int i=0; i<count; i++)
-// { 
-//     if (array[i] == 1) p =p+1;
-// }
-// for (int i=0; i<count; i++)
-// { 
-//     if (array[i] == 0) e =e+1;
-// }
-// if (p>e) Console.WriteLine($"Колличество единиц больше нулей, {p}");
-// else Console.WriteLine($"Колличество нулей больше единиц, {e}");
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int lenght = 8;
+    int index = 0;
+    while (index<lenght)
+{
+    collection[index] = new Random().Next(0,2);
+    index++;
+}
+}
+void PrintArray(int[] array)
+{
+    int count = 8;
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
+int p=0;
+int e=0;
+int count = 8;
+for (int i=0; i<count; i++)
+{ 
+    if (array[i] == 1) p =p+1;
+}
+for (int i=0; i<count; i++)
+{ 
+    if (array[i] == 0) e =e+1;
+}
+if (p>e) Console.WriteLine($"Колличество единиц больше нулей, {p}, True ");
+else Console.WriteLine($"Колличество нулей больше единиц, {e}, False ");
+// потренироватьсь с булл
 // ______________________________________
+
