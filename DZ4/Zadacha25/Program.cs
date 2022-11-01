b1f515feb69486a04ee8c83bd0c6f8dea2a2e4d0
«Vadim <Hazz74@yandex.ru>
2022-11-02 01:03:47 +0500

    ReWrite cod in Zadacha25 and Zadacha27

--- DZ4/Zadacha25/Program.cs
+++ DZ4/Zadacha25/Program.cs
@@ -2,3 +2,16 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.WriteLine("Введите A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите B");
+int b = Convert.ToInt32(Console.ReadLine());
+void MathPow(int a)
+{
+    for (int i = 1; i<2; i++)
+    {
+    Console.WriteLine($"{a}^{b} = {(long)Math.Pow(a, b):N0}");
+    }
+}
+MathPow(a);
--- DZ4/Zadacha27/Program.cs
+++ DZ4/Zadacha27/Program.cs
@@ -3,3 +3,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.WriteLine("Введите n");
+int x = Convert.ToInt32(Console.ReadLine());
+int n = (int)Math.Log10(x) + 1;
+void SumNumbers(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + x%10;
+        x = x/10;
+       
+    }
+    Console.WriteLine($"Сумма всех цифр числа x равна {sum}");
+}
+SumNumbers(n);
+
+// int sum = 0;
+// for (int i = 1; i <= n; i++)
+// {
+//    sum = sum + x%10;
+//    x = x/10;
+// }
+// Console.Write($"Сумма всех цифр числа x равна, {sum}");
