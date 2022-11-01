2be888019f965212eaa66c26e2d4770e34db32df
«Vadim <Hazz74@yandex.ru>
2022-11-01 21:24:22 +0500

    Write cod Zadacha19

--- DZ3/Zadacha19/Program.cs
+++ DZ3/Zadacha19/Program.cs
@@ -6,45 +6,18 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-try
-{
+
 Console.WriteLine("Введите пятизначное число");
-int n=Convert.ToInt32 ( Console.ReadLine());
+int n = Convert.ToInt32 (Console.ReadLine());
 int NumLen = (int)Math.Log10(n) + 1;
 Console.WriteLine($"Количество цифр в числе,  {NumLen}");
-if (NumLen <0 && NumLen >=6) Console.WriteLine("Введено число отличное от пятизначного");
-int i = 1;
-int x = 0;
-int y = 0;
+if (NumLen == 5) Console.WriteLine("Введено пятизначное число");
+else Console.WriteLine("Число не пятизначное");
+int a1 = n/10000;
 
-if (n>0 && n<100000)
-   {
-    x = n%10
-    y = 
-    if 
-    x = n%10;
-    Console.Write("Тетья цифра, " + x);
-    
-    Console.Write("bhhbhb, {i}");
-    } 
-while (i <= NumLen)
-{
-   n = n/10; 
-   
-   if (n>99 && n<1000)
-   {
-    x = n%10;
-    Console.Write(n +" ");// Уменьшение числа до 3 символов
-    Console.WriteLine("Тетья цифра, " + x);
-    break;
-   } 
-   i++;
-}
-}
-catch 
-{
-    Console.WriteLine("Введены некорректные данные");
-}
+int a2 = n/1000 - n/10000*10;
+int a3 = n%100/10; 
+int a4 = n%10;
 
-
-
+if (a1==a4 && a2==a3) Console.WriteLine("Число является палиндромом");
+else Console.WriteLine("Число не является палиндромом");
