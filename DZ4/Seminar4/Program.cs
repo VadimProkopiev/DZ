f93d5ddef1cdd31434f90ca60fc830bfc1a211dc
«Vadim <Hazz74@yandex.ru>
2022-11-01 22:28:01 +0500

    writeSeminar4

--- DZ4/Seminar4/Program.cs
+++ DZ4/Seminar4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Введите целое число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите целое число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum=0;
+// for (int i =1; i<=n;i++) sum=sum+i;
+// Console.WriteLine($"Сумма всех чисел от 1 до {n} равна {sum}");
+
+void SumNumbers(int x)
+{
+    int sum=0;
+    for (int i =1; i<=x;i++) 
+        sum=sum+i;
+    Console.WriteLine($"Сумма всех чисел от 1 до {x} равна {sum}");
+}
+
+int SumNumbers2(int x)
+{
+    int sum=0;
+    for (int i =1; i<=x;i++) 
+        sum=sum+i;
+    return sum;
+}
+
+
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+//SumNumbers(n);
+Console.WriteLine(SumNumbers2(n));
