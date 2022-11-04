d51ce8a97a396fefb7222eba2d570fd7e283ae90
«Vadim <Hazz74@yandex.ru>
2022-11-04 18:48:34 +0500

    Write cod Zadacha 38

--- DZ5/Zadacha38/Program.cs
+++ DZ5/Zadacha38/Program.cs
@@ -2,3 +2,38 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+
+int[] arr = {302, 754, 22, 233, 78, 54};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+void FindCount(int[] arr)
+{
+    
+    int max = arr[0];
+    int min = arr[0];
+   for (int i = 0; i < arr.Length; i++)
+   {
+        if (arr[i]>max) max = arr[i];
+
+        if (arr[i]<min) min = arr[i];
+
+   }
+   int dif = max-min;
+   
+   Console.WriteLine($"Минимальное число - {min}");
+   Console.WriteLine($"Максимальное число - {max}");
+   Console.WriteLine($"Разница между максимальным и минимальным числами - {dif}");
+}
+
+PrintArray(arr);
+FindCount(arr);
+
