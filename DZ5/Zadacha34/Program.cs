941ed5be533b922f4663f047a24287996fe0eb44
«Vadim <Hazz74@yandex.ru>
2022-11-04 17:24:44 +0500

    Write cod Zadacha 34

--- DZ5/Zadacha34/Program.cs
+++ DZ5/Zadacha34/Program.cs
@@ -1,3 +1,29 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+int[] CreateFillArray()
+
+{
+    int[] arr = new int[13];
+    for (int i=0;i<13;i++) 
+    {
+        arr[i]= new Random().Next(99,1000);
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+    return arr;
+}
+
+void FindCount(int[] arr)
+{
+    int count=0;
+    foreach (int el in arr)
+    {
+        if (el%2 == 0 ) count++;
+    }
+    Console.WriteLine(count);
+}
+int [] arr = CreateFillArray();
+FindCount(arr); 
+
