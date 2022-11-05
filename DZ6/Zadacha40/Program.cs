39e9cd7db06bf91d242f41c621bf4af46505d6d1
«Vadim <Hazz74@yandex.ru>
2022-11-05 12:39:21 +0500

    Write cod zadacha40

--- DZ6/Zadacha40/Program.cs
+++ DZ6/Zadacha40/Program.cs
@@ -4,17 +4,33 @@
 // по нему - площадь, периметр, значения углов треугольника в 
 // градусах, является ли он прямоугольным, равнобедренным, равносторонним.
 
- Console.WriteLine("Введите первое число ");
-int x=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+double x=Convert.ToInt32 ( Console.ReadLine());
 Console.WriteLine("Введите второе число ");
-int y=Convert.ToInt32 ( Console.ReadLine());
+double y=Convert.ToInt32 ( Console.ReadLine());
 Console.WriteLine("Введите третье число ");
-int z=Convert.ToInt32 ( Console.ReadLine());
+double z=Convert.ToInt32 ( Console.ReadLine());
 
 if (x>y+z || y>x+z || z>x+y) Console.WriteLine("Такой треугольник не существует");
 else Console.WriteLine("Такой треугольник существует");
 { 
-    int p = (x+y+z)/2;                                
-    Console.WriteLine($"Площадь треугольника =, {double S = Math.Sqrt(p*(p-x)*(p-y)*(p-z));}");//S=√(p·(p-a)·(p-b)·(p-c))
-    Console.WriteLine($"Периметр треугольника =, {x+y+z}");
+    double p = (x+y+z)/2; 
+    double S = Math.Sqrt(Math.Abs(p*(p-x)*(p-y)*(p-z))); //S=√(p·(p-a)·(p-b)·(p-c))
+    double alfa = Math.Acos(Math.Abs((Math.Pow(y, 2)+Math.Pow(z, 2)-Math.Pow(x, 2))/(2*z*y)));
+    double alfaDeg = alfa*180/Math.PI;
+    alfaDeg = Math.Round(alfaDeg, 2);
+    double beta = Math.Acos(Math.Abs((Math.Pow(x, 2)+Math.Pow(z, 2)-Math.Pow(y, 2))/(2*z*x)));
+    double betaDeg = beta*180/Math.PI;
+    betaDeg = Math.Round(betaDeg, 2);
+    double gamaDeg = 180 - alfaDeg - betaDeg;
+    gamaDeg = Math.Round(gamaDeg, 2);
+    S = Math.Round(S, 2);
+    Console.WriteLine($"Площадь треугольника = {S}");
+    Console.WriteLine($"Периметр треугольника = {x+y+z}");
+    Console.WriteLine($"Угол alfa треугольника = {alfaDeg}");
+    Console.WriteLine($"Угол beta треугольника = {betaDeg}");
+    Console.WriteLine($"Угол gama треугольника = {gamaDeg}");
+    if (alfaDeg == 90 || betaDeg == 90 || gamaDeg == 90) Console.WriteLine("Такой треугольник является прямоугольным");
+    if (x == y && y == z && z== x) Console.WriteLine("Такой треугольник является равносторонним");
+    if (alfaDeg==betaDeg || betaDeg==gamaDeg || gamaDeg==alfaDeg) Console.WriteLine("Такой треугольник является равнобедренным");
 }
