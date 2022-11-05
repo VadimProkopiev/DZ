3ac1e8c3e539afa924e8920dc24e20731b3b271c
«Vadim <Hazz74@yandex.ru>
2022-11-05 14:19:32 +0500

    Write cod Zadacha 41

--- DZ6/Zadacha41/Program.cs
+++ DZ6/Zadacha41/Program.cs
@@ -3,5 +3,17 @@
 
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
-Console.WriteLine("Введите колличество M чисел, ");
-double M=Convert.ToInt32 ( Console.ReadLine());
+
+Console.Write("Введите с клавиатуры М числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Чисел больше нуля: {count}");
