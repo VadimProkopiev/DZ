708473ab482feed1055c16527f88cda82290a409
«Vadim <Hazz74@yandex.ru>
2022-11-05 15:07:25 +0500

    Write cod Zadacha 43

--- DZ6/Zadacha43/Program.cs
+++ DZ6/Zadacha43/Program.cs
@@ -4,21 +4,42 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+void FindPoint ()
+{
+    Console.WriteLine("Введите значение b1, ");
+    double b1=Convert.ToInt32 ( Console.ReadLine());
+    Console.WriteLine("Введите значение k1, ");
+    double k1=Convert.ToInt32 ( Console.ReadLine());
+    Console.WriteLine("Введите значение b2, ");
+    double b2=Convert.ToInt32 ( Console.ReadLine());
+    Console.WriteLine("Введите значение k2, ");
+    double k2=Convert.ToInt32 ( Console.ReadLine());
 
-Console.WriteLine("Введите значение b1, ");
-double b1=Convert.ToInt32 ( Console.ReadLine());
-Console.WriteLine("Введите значение k1, ");
-double k1=Convert.ToInt32 ( Console.ReadLine());
-Console.WriteLine("Введите значение b2, ");
-double b2=Convert.ToInt32 ( Console.ReadLine());
-Console.WriteLine("Введите значение k2, ");
-double k2=Convert.ToInt32 ( Console.ReadLine());
 
-
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
  
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
  
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+    Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
+FindPoint();
+
+// Console.WriteLine("Введите значение b1, ");
+// double b1=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите значение k1, ");
+// double k1=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите значение b2, ");
+// double b2=Convert.ToInt32 ( Console.ReadLine());
+// Console.WriteLine("Введите значение k2, ");
+// double k2=Convert.ToInt32 ( Console.ReadLine());
+
+
+//double x = (b2 - b1) / (k1 - k2);
+//double y = k1 * x + b1;
+ 
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+ 
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
