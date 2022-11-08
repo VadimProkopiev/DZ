b2eef9b082ac576cde509b4058d662821c137f64
«Vadim <Hazz74@yandex.ru>
2022-11-09 00:56:45 +0500

    Add cod Seminar 8

--- DZ7/ZadachaHardSort/Program.cs
+++ DZ7/ZadachaHardSort/Program.cs
@@ -8,3 +8,99 @@
 // После сортировки
 // 1 2 3 4
 // 5 7 9 10
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] Fillimageay(int m, int n)
+{
+    int[,] mat = new int[m,n];
+    for (int i=0;i<mat.GetLength(0);i++)
+    {
+        for (int j=0;j<mat.GetLength(1);j++)
+        {
+            mat[i,j] = new Random().Next(0, 10);
+        }
+    }
+    Console.WriteLine();
+    return mat;
+}
+
+void Printimageay(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            Console.Write($"{image[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SelectionSortRow(int[,] image)
+{
+    for (int i =0; i< image.GetLength(0); i++)
+    {
+        for ( int j = 0; j < image.GetLength(1) ; j++)
+        {
+            for (int k = 0; k < image.GetLength(1) - j-1 ;k++) 
+                {
+                    if (image[i, k] > image[i, k + 1]) 
+                    {
+                        int t = image[i, k];
+                        image[i, k] = image[i, k + 1];
+                        image[i, k + 1] = t;
+                    }
+                }
+        }
+    }
+}
+
+void SelectionSortCol(int[,] image)
+{
+    for (int j =0; j< image.GetLength(1); j++)
+    {
+        for ( int i = 0; i < image.GetLength(0) ; i++)
+        {
+            for (int k = 0; k < image.GetLength(1) - i-1 ;k++) 
+                {
+                   for (int l =0; l< image.GetLength(1);l++)
+                    if (image[k, l] > image[k+1, l]) 
+                    {
+                        int t = image[k, l];
+                        image[k, l] = image[k+1, l];
+                        image[k + 1, l ] = t;
+                    }
+                }
+        }
+    }
+}
+void transpose(int[, ] mat)
+    {
+        for (int i = 0; i < mat.GetLength(0); i++) 
+        {
+            for (int j = i + 1; j < mat.GetLength(1); j++) 
+            {
+                int temp = mat[i, j];
+                mat[i, j] = mat[j, i];
+                mat[j, i] = temp;
+            }
+        }
+    }
+
+int [,] mas = Fillimageay(m, n);
+Printimageay(mas);
+Console.WriteLine();
+
+
+SelectionSortRow(mas);
+
+
+Printimageay(mas);
+
+
+// ________________________________________________________________________
--- DZ8/Seminar8/Program.cs
+++ DZ8/Seminar8/Program.cs
@@ -1,2 +1,178 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] Fillimageay(int m, int n)
+// {
+//     int[,] mat = new int[m,n];
+//     for (int i=0;i<mat.GetLength(0);i++)
+//     {
+//         for (int j=0;j<mat.GetLength(1);j++)
+//         {
+//             mat[i,j] = new Random().Next(0, 10);
+//         }
+//     }
+//     Console.WriteLine();
+//     return mat;
+// }
+
+// void Printimageay(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int [,] mas = Fillimageay(m, n);
+// Printimageay(mas);
+// Console.WriteLine();
+
+// for (int j = 0; j < mas.GetLength(1); j++)
+// {
+//     int temp = mas[0, j];
+//     mas[0, j] = mas[mas.GetLength(0)-1, j];
+//     mas[mas.GetLength(0)-1, j]  = temp;
+// }
+
+// Printimageay(mas);
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (m==n)
+// { 
+//     int[,] Fillimageay(int m, int n)
+//     {
+//         int[,] mat = new int[m,n];
+//         for (int i=0;i<mat.GetLength(0);i++)
+//         {
+//             for (int j=0;j<mat.GetLength(1);j++)
+//             {
+//                 mat[i,j] = new Random().Next(0, 10);
+//             }
+//         }
+//     Console.WriteLine();
+//     return mat;
+//     }
+
+//     void Printimageay(int[,] image)
+//     {
+//         for (int i = 0; i < image.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < image.GetLength(1); j++)
+//                 {
+//                 Console.Write($"{image[i, j]} \t");
+//                 }
+//             Console.WriteLine();
+//         }
+//     }
+
+//     int [,] mas = Fillimageay(m, n);
+//     Printimageay(mas);
+//     Console.WriteLine();
+
+//     int[,] Fillimage(int n, int m)
+//     {
+//     int[,] mas2 = new int[m,n];
+//     for (int i=0;i<mas2.GetLength(0);i++)
+//     {
+//         for (int j=0;j<mas2.GetLength(1);j++)
+//         {
+//             mas2[j,i] = mas[i,j];
+            
+//         }
+//     }
+//     Console.WriteLine();
+//     return mas2;
+//     }
+//     int[,] mas2 =Fillimage(n,m);
+//     Printimageay(mas2);
+
+
+// }
+// else Console.WriteLine("Невозможно заменить строки на столбцы");
+// _________________________________________________________________________________________
+
+
+// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+    int[,] Fillimageay(int m, int n)
+    {
+        int[,] mat = new int[m,n];
+        for (int i=0;i<mat.GetLength(0);i++)
+        {
+            for (int j=0;j<mat.GetLength(1);j++)
+            {
+                mat[i,j] = new Random().Next(0, 10);
+            }
+        }
+    Console.WriteLine();
+    return mat;
+    }
+
+    void Printimageay(int[,] image)
+    {
+        for (int i = 0; i < image.GetLength(0); i++)
+        {
+            for (int j = 0; j < image.GetLength(1); j++)
+                {
+                Console.Write($"{image[i, j]} \t");
+                }
+            Console.WriteLine();
+        }
+    }
+
+    int [,] mas = Fillimageay(m, n);
+    Printimageay(mas);
+    Console.WriteLine();
+
+    int[] mas2 = new int[10];
+
+        for (int i=0;i<mas.GetLength(0);i++)
+        {
+           for (int j = 0; j < mas.GetLength(1); j++) 
+            
+            mas2[mas[i,j]]++;
+                     
+                          
+        }
+ 
+        for (int i = 0; i <mas2.Length; i++)
+        {
+            
+            Console.Write($"Число {i} встречается  {mas2[i]}  раз");
+            
+            Console.WriteLine();
+        }
+
+// ___________________________________________________________________________________________
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец,
+//  на пересечении которых расположен наименьший элемент массива.
