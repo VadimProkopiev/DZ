e3fe98c450a9115cb09327fdf3a1fb8f9ba8a489
«Vadim <Hazz74@yandex.ru>
2022-11-08 09:54:08 +0500

    Write cod Zadacha 47

--- DZ7/Zadacha50/Program.cs
+++ DZ7/Zadacha50/Program.cs
@@ -10,3 +10,39 @@
 // 8 4 2 4
 
 // 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] FillArray(int m, int n)
+{
+    Random rand = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(-5, 5) + rand.NextDouble() * 5;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            image[i, j] = Math.Round(image[i, j], 1);
+            Console.Write($"{image[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] mas = FillArray(m, n);
+PrintArray(mas);
+Console.WriteLine();
