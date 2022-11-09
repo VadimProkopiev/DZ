ee07b4538cadeb3ee049afba13c6760c963275b7
«Vadim <Hazz74@yandex.ru>
2022-11-09 13:18:56 +0500

    Add cod Zadacha 54

--- DZ8/Zadacha54/Program.cs
+++ DZ8/Zadacha54/Program.cs
@@ -9,3 +9,66 @@
 // 9 5 3 2
 // 8 4 4 2
 
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] Fillimageay(int m, int n)
+{
+    int[,] mat = new int[m,n];
+    for (int i=0;i<mat.GetLength(0);i++)
+    {
+        for (int j=0;j<mat.GetLength(1);j++)
+        {
+            mat[i,j] = new Random().Next(0, 10);
+        }
+    }
+    Console.WriteLine();
+    return mat;
+}
+
+void Printimageay(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            Console.Write($"{image[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SelectionSortRow(int[,] image)
+{
+    for (int i =0; i< image.GetLength(0); i++)
+    {
+        for ( int j = 0; j < image.GetLength(1) ; j++)
+        {
+            for (int k = 0; k < image.GetLength(1) - j-1 ;k++) 
+                {
+                    if (image[i, k] < image[i, k + 1]) 
+                    {
+                        int t = image[i, k];
+                        image[i, k] = image[i, k + 1];
+                        image[i, k + 1] = t;
+                    }
+                }
+        }
+    }
+}
+
+
+
+
+int [,] mas = Fillimageay(m, n);
+Printimageay(mas);
+Console.WriteLine();
+
+
+SelectionSortRow(mas);
+
+
+Printimageay(mas);
