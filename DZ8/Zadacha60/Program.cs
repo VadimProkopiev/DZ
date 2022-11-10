55a07117d802aaf7029c9a1d691d457ebb779ba8
«Vadim <Hazz74@yandex.ru>
2022-11-10 12:31:41 +0500

    Fixed Cod Zadacha 60

--- DZ8/Zadacha60/Program.cs
+++ DZ8/Zadacha60/Program.cs
@@ -12,24 +12,57 @@ Console.WriteLine("Введите ширину трехмерного масси
 int y = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите глубину трехмерного массива");
 int z = Convert.ToInt32(Console.ReadLine());
-
+Random rnd = new Random();
+int [,,] mas = Fillimageay(x,y,z);
+Printimageay(mas);
 
 int[,,] Fillimageay(int x, int y, int z)
 {
+    int min = 10;
+    int max = 100;
     int[,,] mas = new int[x,y,z];
-    for (int i=0;i<mas.GetLength(0);i++)
     {
-        for (int j=0;j<mas.GetLength(1);j++)
+        for (int i=0;i<mas.GetLength(0);i++)
         {
-            for (int k=0;k<mas.GetLength(2);k++)
+            for (int j=0;j<mas.GetLength(1);j++)
             {
-               mas[i,j,k] = new Random().Next(0, 100); 
+                for (int k = 0; k < mas.GetLength(2); k++) 
+                { 
+                    mas[i, j, k] = GetUniqueValue(mas, min, max, i, j, k);
+                }
+                          
             }
-            
+        }
+    Console.WriteLine(); 
+    }
+    
+    return mas;
+}
+
+int GetUniqueValue(int[,,] mas, int min, int max, int i, int j, int k)
+{
+    
+    int value = default;
+    bool exist = true;
+    while (exist)
+    {
+        bool _break = false;
+        value = rnd.Next(min, max + 1);
+        for (int i1 = 0; i1 < mas.GetLength(0); i1++)
+        {
+            if (_break) { break; }
+            for (int j1 = 0; j1 < mas.GetLength(1); j1++)
+            {
+                if (_break) { break; }
+                for (int k1 = 0; k1 < mas.GetLength(2); k1++)
+                {
+                    if (mas[i1, j1, k1] == value) { _break = true; break; }
+                    if (i1 == i && j1 == j && k1 == k) { exist = false; }
+                }
+            }
         }
     }
-    Console.WriteLine();
-    return mas;
+    return value;
 }
 
 void Printimageay(int[,,] image)
@@ -38,51 +71,15 @@ void Printimageay(int[,,] image)
     {
         for (int j = 0; j < image.GetLength(1); j++)
         {
-            for (int k=0;k<image.GetLength(2);k++)
-            {
-               Console.Write($"{image[i, j, k]} \t");
-               Console.WriteLine($"число массива = {image[i, j, k]} индексы числа ({i}, {j}, {k}) ");
-            }
-            
+            Console.Write("|");
+            for (int k = 0; k < mas.GetLength(2); k++) { Console.Write($"{mas[i, j, k],1}({i},{j},{k})|"); }
+            Console.WriteLine();
+                       
         }
         Console.WriteLine();
     }
 }
-int [,,] mas = Fillimageay(x,y,z);
-Printimageay(mas);
 
 
 
-// void SortSumRow(int[,] mas)
-// {
-//     int index = 0, minsum = 0;
-//     for (int i = 0; i < mas.GetLength(0); i++)
-//     {
-//         int sum = 0;
-//         for (int j = 0; j < mas.GetLength(1); j++)
-//         {
-//             sum += mas[i, j];
-//         }
-//         Console.WriteLine($"Сумма {i + 1} строки = {sum}");
-//         if (i == 0)
-//         {
-//             minsum = sum;
-//         }
-//         else if (sum < minsum)
-//         {
-//             minsum = sum;
-//             index = i;
-//         }
-//     }
-//     string line = string.Empty;
-//     for (int j = 0; j < mas.GetLength(1); j++)
-//     {
-//         line += mas[index, j] + " ";
-//     }
-//     Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. индекс = {index} ");
-// }
 
-// SortSumRow(mas);
-
-
-// Printimageay(mas);
