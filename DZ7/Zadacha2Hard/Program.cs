15402c8896ebb81f75cf92e3d03b68f65042d26f
«Vadim <Hazz74@yandex.ru>
2022-11-11 11:55:14 +0500

    Add cod Zadacha 2 hard

--- DZ7/Zadacha2Hard/Program.cs
+++ DZ7/Zadacha2Hard/Program.cs
@@ -4,3 +4,69 @@
 // Перемешать случайным образом элементы массива, причем чтобы каждый гарантированно 
 // переместился на другое место и выполнить это за m*n / 2 итераций. То есть если массив три на четыре,
 // то надо выполнить не более 6 итераций. И далее в конце опять вывести на экран как таблицу.
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m*n%2==0)
+{
+    Console.WriteLine("Количество элементов четное.");
+    int [,] mas = Fillimageay(m, n);
+    Printimageay(mas);
+    Console.WriteLine();
+
+    int[,] Fillimageay(int m, int n)
+    {
+        int[,] image = new int[m,n];
+        for (int i=0;i<image.GetLength(0);i++)
+        {
+            for (int j=0;j<image.GetLength(1);j++)
+            {
+                image[i,j] = new Random().Next(0, 10);
+            }
+        }
+        Console.WriteLine();
+        return image;
+    }
+
+    void Printimageay(int[,] image)
+    {
+        for (int i = 0; i < image.GetLength(0); i++)
+        {
+            for (int j = 0; j < image.GetLength(1); j++)
+            {
+                Console.Write($"{image[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+    void SelectionSortRow(int[,] mas)
+    {
+        for (int i =0; i< mas.GetLength(0); i++)
+        {
+            for ( int j = 0; j < mas.GetLength(1) ; j++)
+            {
+                for (int k = 0; k < mas.GetLength(1) - j-1 ;k++) 
+                {
+                    if (mas[i, k] < mas[i, k + 1]) 
+                    {
+                        int t = mas[i, k];
+                        mas[i, k] = mas[i, k + 1];
+                        mas[i, k + 1] = t;
+                        
+                    }
+                }
+                
+            }
+            
+        }
+    
+    }
+    SelectionSortRow(mas);
+    Printimageay(mas);
+
+
+
+}
+else Console.WriteLine("Количество элементов нечетное.");
